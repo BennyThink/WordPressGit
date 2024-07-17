52be3869f4ec7fbb6ea7551a34dbfec64efd826b
Benny <benny.think@gmail.com>
2024-07-17 10:52:13 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -344,7 +344,7 @@ function footerScript()
     if (!is_admin()) {
         wp_deregister_script('jquery');
         if (git_get_option('git_jqcdn') == 'git_jqcdn_qiniu') {
-            wp_register_script('jquery', '//cdn.staticfile.org/jquery/1.8.3/jquery.min.js', false, '1.0', false);
+            wp_register_script('jquery', '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js', false, '1.0', false);
         } elseif (git_get_option('git_jqcdn') == 'git_jqcdn_upai') {
             wp_register_script('jquery', '//upcdn.b0.upaiyun.com/libs/jquery/jquery-1.8.3.min.js', false, '1.0', false);
         } elseif (git_get_option('git_jqcdn') == 'git_jqcdn_sae') {
